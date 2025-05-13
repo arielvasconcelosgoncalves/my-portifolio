e02e7aab38cb667389b1932d8873031d0dc8d2b6
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2025-05-13 18:43:35 -0300

    feat: add toolbar and styled

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,20 +1,25 @@
-import {Container, Grid, styled, Typography} from "@mui/material"
+import {Box, Container, Grid, styled, Typography} from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.jpg"
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import EmailIcon from '@mui/icons-material/Email';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 
 const Hero = () =>{
 
     const StyledHero = styled("div")(({theme}) => ({
         backgroundColor: theme.palette.primary.main,
-        height: "100vh"
+        height: "100vh",
+        display: "flex",
+        alignItems: "center"
+       
     }))
 
-    const StyledImg = styled("img")(() => ({
-        width: "100%",
-        borderRadius: "50%"
+    const StyledImg = styled("img")(({theme}) => ({
+        width: "80%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
     }))
 
     return (
@@ -22,15 +27,22 @@ const Hero = () =>{
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid size={{ xs: 12, sm: 4 }}>
-                        <StyledImg src={Avatar} alt="" />
+                    <Grid size={{ xs: 12, sm: 4.5 }}>
+                        <Box position="relative">
+                            <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                <AnimatedBackground />
+                            </Box>
+                            <Box position="relative" textAlign="center">
+                                <StyledImg src={Avatar} alt="" />
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid size={{ xs: 12, sm: 8 }} display="flex" flexDirection="column" justifyContent="center">
-                        <Typography color="primary.contrastText" variant="h1" textAlign="center">Ariel Vasconcelos</Typography>
+                    <Grid size={{ xs: 12, sm: 7.5 }} display="flex" flexDirection="column" justifyContent="center">
+                        <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Ariel Vasconcelos</Typography>
                         <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Computer Engineer</Typography>                       
                     </Grid>
                 </Grid>
-                <Grid container display="flex" justifyContent="end" spacing={3}>
+                <Grid container display="flex" justifyContent="end" spacing={3} pt={3}>
                     <Grid size={{ xs: 12, sm: 4 }} display="flex" justifyContent="center">
                         <StyledButton>
                             <FileDownloadIcon/>
